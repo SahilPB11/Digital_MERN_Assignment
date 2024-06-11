f7b7170b27febc54429aef78e7c4e66b1c786a21
Sahil <garg07825@gmail.com>
2024-06-11 11:09:48 +0530

    connected with database

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
+// here we are importing the index.js from the server folder
 import { app } from "./index.js";
+// here we are importing the connect_with_database function from the config folder
+import { connect_with_database } from "./config/Database.js";
 
 const Port = process.env.PORT || 3000;
 
-// here first we will coonect with database then we will start the server
-
-app.listen(Port, () => {
-  console.log(`server is running on port ${Port}`);
-});
+// here first we will connect with database then we will start the server
+connect_with_database()
+  .then(() => {
+    app.listen(Port, () => {
+      console.log(`Server is running on port ${Port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
